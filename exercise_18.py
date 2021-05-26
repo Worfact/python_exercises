5afda0c604efb8c38cdfe68dc44af6ffb7def177
Worfact <worfact@gmail.com>
2021-05-26 11:56:45 +0200

    Update code with the test list

--- exercise_18.py
+++ exercise_18.py
@@ -2,6 +2,12 @@ import sys
 
 password_list = input().split(',')
 
+def test_launcher(x):
+    test_list = (length_test, uppercase_test, lowercase_test, number_test, special_test)
+    for test in test_list:
+            if test(x) != True:
+                return(False)
+
 def length_test(x):
     if len(x) > 5 and len(x) < 13:
         return(True)
@@ -33,18 +39,11 @@ def special_test(x):
             break
 
 def password_test(password_list):
+    result_list = []
     for x in password_list:
-        if length_test(x) != True:
-            continue
-        if uppercase_test(x) != True:
-            continue
-        if lowercase_test(x) != True:
-            continue
-        if number_test(x) != True:
-            continue
-        if special_test(x) != True:
-            continue
-        print(x)
+        if test_launcher(x) != False:
+            result_list.append(x)
+    print(', '.join(result_list))
 
 
 password_test(password_list)
